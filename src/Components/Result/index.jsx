b26f6792b619ffaee88aa6479f58f9bb55a1180b
kevinhouguet <k.houguet@gmail.com>
2023-05-13 21:19:09 +0200

    feat(result): add component result

--- src/Components/Result/index.jsx
+++ src/Components/Result/index.jsx
@@ -1,16 +1,22 @@
-import './style.css'
+import './style.scss'
+import { useState } from 'react';
 
 const Result = () => {
+
+  const [days, setDay] = useState('--')
+  const [months, setMonth] = useState('--')
+  const [years, setYear] = useState('38')
+
   return (
     <div className="result__content">
       <div className="result__content__years">
-        <span className="result__content__years__nb">--</span> years
+        <span className="result__content__years__nb">{years}</span> years
       </div>
       <div className="result__content__month">
-        <span className="result__content__months__nb">--</span> months
+        <span className="result__content__months__nb">{months}</span> months
       </div>
       <div className="result__content__days">
-        <span className="result__content__days__nb">--</span> days
+        <span className="result__content__days__nb">{days}</span> days
       </div>
     </div>
   )
