08f5969c39756ecdedf7a864bdcb443e5ff3b560
kevinhouguet <k.houguet@gmail.com>
2023-05-13 19:29:54 +0200

    :rocket: feat(containerApp): add design to container

--- src/App.jsx
+++ src/App.jsx
@@ -35,10 +35,10 @@ const App = () => {
         </div>
       </div>
 
-      <footer className="attribution">
+      {/* <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
         Coded by <a href="#">Your Name Here</a>.
-      </footer>
+      </footer> */}
     </main>
   )
 }
