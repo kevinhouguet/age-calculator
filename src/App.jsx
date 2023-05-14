86d53433e54320e2abaaa1ade47e9a520efb42c5
kevinhouguet <k.houguet@gmail.com>
2023-05-14 13:17:06 +0200

    :sparkles:feat(app): add desktop responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ const App = () => {
       </main>
       <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/kevinhouguet">kevinHouguet</a>.
       </footer>
     </>
   )
